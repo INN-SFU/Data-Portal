97460a817c7afd1c94549aef5633e6cda3b46edd
Patrick Mahon <pmahon@sfu.ca>
2024-07-23 19:56:47 +0000

    updated service endpoint function signature and api docs

--- api/v0_1/endpoints/service/admin.py
+++ api/v0_1/endpoints/service/admin.py
@@ -1,13 +1,12 @@
 import os
 from uuid import uuid5, NAMESPACE_DNS
 
-from fastapi import Request, Depends, HTTPException, APIRouter, status
+from fastapi import Depends, HTTPException, APIRouter, status, Query
 from fastapi.security import HTTPBasic
 from fastapi.templating import Jinja2Templates
 
 from core.connectivity import agents
 from core.data_access_manager import dam
-
 from api.v0_1.endpoints.utils.server import is_admin
 
 security = HTTPBasic()
@@ -15,22 +14,30 @@ admin_router = APIRouter(prefix='/admin')
 templates = Jinja2Templates(directory=os.getenv('JINJA_TEMPLATES'))
 
 
-# Test
 @admin_router.get("/test", dependencies=[Depends(is_admin)])
 async def admin_route():
+    """
+    Handler for the admin test route.
+
+    Returns:
+    - **dict**: A dictionary with a welcome message for the administrator.
+    """
     return {"message": "Welcome, administrator!"}
 
 
-# User View and Control
 @admin_router.get("/user/{uid}", dependencies=[Depends(is_admin)])
 async def get_user_(uid: str):
     """
     Handler for the get user endpoint.
 
-    :param uid: The user ID.
-    :type uid: str
-    :return: The rendered home.html template.
-    :rtype: templates.TemplateResponse
+    Parameters:
+    - **uid** (str): The user ID or "list" to retrieve all users.
+
+    Returns:
+    - **dict**: The user information if found, or a list of users if "list" is passed as uid.
+
+    Raises:
+    - **HTTPException**: If the user is not found.
     """
     if uid == "list":
         return dam.get_users()
@@ -40,31 +47,28 @@ async def get_user_(uid: str):
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
 
-    return dam.get_user(uid)
+    return user
 
 
 @admin_router.put("/user/", dependencies=[Depends(is_admin)])
-async def add_user_(request: Request):
+async def add_user_(uid: str = Query(...), role: str = Query(...)):
     """
     Add a user.
 
-    :param uid: The unique ID of the user to be added (email).
-    :param role: Their role (user or admin)
-    :param request: The incoming request object.
-    :type request: Request
-    :return: The rendered add_user.html template.
-    :rtype: templates.TemplateResponse
-    """
-    try:
-        uid = request.query_params.get('uid')
-        role = request.query_params.get('role')
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Unable to retrieve uid or role: {e.__str__}")
+    Parameters:
+    - **uid** (str): The unique ID of the user to be added (email).
+    - **role** (str): The role of the user ("user" or "admin").
 
+    Returns:
+    - **dict**: A dictionary with the success message and the user's secret key.
+
+    Raises:
+    - **HTTPException**: If there is an error adding the user.
+    """
     uuid = uuid5(NAMESPACE_DNS, uid)
 
-    if role != "admin" or "user":
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid role. \"admin\" or \"user\".")
+    if role not in ["admin", "user"]:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid role. Must be "admin" or "user".')
 
     try:
         user_secret_key = dam.add_user(uid, uuid, role)
@@ -79,9 +83,14 @@ async def remove_user(uid: str):
     """
     Delete a user and all their associated data.
 
-    :param uid: The unique ID of the user to be removed.
-    :return: A dictionary with the success message if the user is removed successfully.
-    :raises HTTPException: If there is an error removing the user.
+    Parameters:
+    - **uid** (str): The unique ID of the user to be removed.
+
+    Returns:
+    - **dict**: A dictionary with the success message if the user is removed successfully.
+
+    Raises:
+    - **HTTPException**: If there is an error removing the user.
     """
     try:
         result = dam.remove_user(uid)
@@ -90,45 +99,53 @@ async def remove_user(uid: str):
         else:
             raise ValueError("Failed to remove user.")
     except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.__str__())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
 
-# Policy View and Control
 @admin_router.get("/policy", dependencies=[Depends(is_admin)])
-async def get_policies(request: Request):
+async def get_policies(uid: str = Query(None), access_point: str = Query(None), resource: str = Query(None),
+                       action: str = Query(None)):
     """
     Retrieve a policy.
 
-    :return: The rendered get_policies.html template.
-    :rtype: te
+    Parameters:
+    - **uid** (str, optional): The user ID.
+    - **access_point** (str, optional): The access point.
+    - **resource** (str, optional): The resource.
+    - **action** (str, optional): The action.
+
+    Returns:
+    - **dict**: A dictionary with the policy information if found.
+
+    Raises:
+    - **HTTPException**: If the policy is not found.
     """
-    uid = request.query_params.get("uid")
-    access_point = request.query_params.get("access_point")
-    resource = request.query_params.get("resource")
-    action = request.query_params.get("action")
     policy = dam.enforcer.get_policy(uid, access_point, resource, action)
 
     if policy is None:
-        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={"failed": "Policy not found."})
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Policy not found.")
 
     return {"policy": policy}
 
 
 @admin_router.put("/policy", dependencies=[Depends(is_admin)])
-async def add_policy(request: Request):
+async def add_policy(uid: str = Query(...), access_point: str = Query(...), resource: str = Query(...),
+                     action: str = Query(...)):
     """
     Add a policy.
 
-    :param request: The incoming request object.
-    :type request: Request
-    :return: The rendered add_policy.html template.
-    :rtype: templates.TemplateResponse
-    """
-    uid = request.query_params.get("uid")
-    access_point = request.query_params.get("access_point")
-    resource = request.query_params.get("resource")
-    action = request.query_params.get("action")
+    Parameters:
+    - **uid** (str): The user ID.
+    - **access_point** (str): The access point.
+    - **resource** (str): The resource.
+    - **action** (str): The action.
 
+    Returns:
+    - **dict**: A dictionary with the success message if the policy is added successfully.
+
+    Raises:
+    - **HTTPException**: If there is an error adding the policy.
+    """
     # Check if the user exists
     if uid not in dam.get_users():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
@@ -139,26 +156,29 @@ async def add_policy(request: Request):
     try:
         dam.add_user_policy(uid, access_point, resource, action)
     except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.__str__())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
     return {"success": "Policy added successfully."}
 
 
 @admin_router.delete("/policy", dependencies=[Depends(is_admin)])
-async def remove_policy(request: Request):
+async def remove_policy(uid: str = Query(...), access_point: str = Query(...), resource: str = Query(...),
+                        action: str = Query(...)):
     """
     Remove a policy.
 
-    :param request: The incoming request object.
-    :type request: Request
-    :return: The rendered remove_policy.html template.
-    :rtype: templates.TemplateResponse
-    """
-    uid = request.query_params.get("uid")
-    access_point = request.query_params.get("access_point")
-    resource = request.query_params.get("resource")
-    action = request.query_params.get("action")
+    Parameters:
+    - **uid** (str): The user ID.
+    - **access_point** (str): The access point.
+    - **resource** (str): The resource.
+    - **action** (str): The action.
 
+    Returns:
+    - **dict**: A dictionary with the success message if the policy is removed successfully.
+
+    Raises:
+    - **HTTPException**: If there is an error removing the policy.
+    """
     try:
         result = dam.remove_policy(uid, access_point, resource, action)
         if result:
@@ -167,4 +187,4 @@ async def remove_policy(request: Request):
             raise ValueError("Failed to remove policy.")
 
     except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e.__str__())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
