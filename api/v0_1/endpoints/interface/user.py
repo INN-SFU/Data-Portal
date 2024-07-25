888ba9a2b3581439bb90dd049503b54cb6e7c14e
Patrick Mahon <pmahon@sfu.ca>
2024-07-25 18:01:21 +0000

    Updated documentation; removed sub router prefixes; standardized service endpoints to return JSONResponses

--- api/v0_1/endpoints/interface/user.py
+++ api/v0_1/endpoints/interface/user.py
@@ -4,6 +4,7 @@ from fastapi import Request, Depends, APIRouter, HTTPException, status, Query
 from fastapi.security import HTTPBasic
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from treelib import node
 
 from core.connectivity import agents
 from core.data_access_manager import dam
@@ -16,7 +17,7 @@ user_ui_router = APIRouter(prefix='/user')
 templates = Jinja2Templates(directory=os.getenv('JINJA_TEMPLATES'))
 
 
-@user_ui_router.get("/home", response_class=HTMLResponse)
+@user_ui_router.get("/home", response_class=HTMLResponse, dependencies=[Depends(validate_credentials)])
 def user_home(request: Request):
     """
     Render the user home page.
@@ -42,10 +43,23 @@ def upload_form(request: Request, uid: str = Depends(validate_credentials)):
     Returns:
     - **TemplateResponse**: The rendered upload form page.
     """
-    assets = {}
-    for agent in agents:
-        assets[agent.access_point_slug] = convert_file_tree_to_dict(agent.get_user_file_tree(uid, 'write', dam))
-    return templates.TemplateResponse("/assets/upload.html", {"request": request, "assets": assets})
+    # Get all storage access points user has read access to
+    access_points = set([policy[1] for policy in dam.get_user_policies(uid)])
+
+    assets = dict.fromkeys(access_points)
+
+    # Loop through all storage end points
+    for agent_slug in access_points:
+        # Filter based on read access
+        def node_filter(n: node):
+            vals = (uid, agent_slug, n.identifier, 'write')
+            return dam.enforcer.enforce(*vals)
+
+        # Add the filtered file tree to the
+        assets[agents[agent_slug].access_point_slug] = (
+            convert_file_tree_to_dict(agents[agent_slug].filter_file_tree(node_filter)))
+
+    return templates.TemplateResponse("/assets/download.html", {"request": request, "assets": assets})
 
 
 @user_ui_router.get("/home/download", response_class=HTMLResponse)
@@ -60,9 +74,22 @@ def download_form(request: Request, uid: str = Depends(validate_credentials)):
     Returns:
     - **TemplateResponse**: The rendered download form page.
     """
-    assets = {}
-    for agent in agents:
-        assets[agent.access_point_slug] = convert_file_tree_to_dict(agent.get_user_file_tree(uid, 'read', dam))
+    # Get all storage access points user has read access to
+    access_points = set([policy[1] for policy in dam.get_user_policies(uid)])
+
+    assets = dict.fromkeys(access_points)
+
+    # Loop through all storage end points
+    for agent_slug in access_points:
+        # Filter based on read access
+        def node_filter(n: node):
+            vals = (uid, agent_slug, n.identifier, 'read')
+            return dam.enforcer.enforce(*vals)
+
+        # Add the filtered file tree to the
+        assets[agents[agent_slug].access_point_slug] = (
+            convert_file_tree_to_dict(agents[agent_slug].filter_file_tree(node_filter)))
+
     return templates.TemplateResponse("/assets/download.html", {"request": request, "assets": assets})
 
 
@@ -85,15 +112,15 @@ def retrieve_asset(request: Request, uid: str = Depends(validate_credentials)):
     access_point = request.query_params.get("access_point")
 
     if dam.validate_user_access(uid, access_point, resource, "read"):
-        for agent in agents:
-            if agent.access_point_slug == access_point:
-                try:
-                    presigned_urls, object_keys = agent.generate_access_links(resource, 'GET', 3600)
-                    return templates.TemplateResponse("download.html",
-                                                      {"request": request, "presigned_urls": presigned_urls,
-                                                       "object_keys": object_keys})
-                except ValueError as e:
-                    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+        try:
+            agent = agents[access_point]
+        except KeyError:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Access point {access_point} not found.")
+
+        presigned_urls, object_keys = agent.generate_access_links(resource, 'GET', 3600)
+        return templates.TemplateResponse("download.html",
+                                          {"request": request, "presigned_urls": presigned_urls,
+                                           "object_keys": object_keys})
     else:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="User does not have read access to this resource")
--- api/v0_1/endpoints/service/user.py
+++ api/v0_1/endpoints/service/user.py
@@ -1,7 +1,10 @@
 import logging
 
 from fastapi import HTTPException, APIRouter, Depends, status, Query
+from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBasic
+from starlette.responses import JSONResponse
+
 from api.v0_1.endpoints.utils.server import validate_credentials
 
 from core.connectivity import agents
@@ -14,28 +17,39 @@ logger = logging.getLogger("uvicorn")
 
 
 @user_router.get("/assets", dependencies=[Depends(validate_credentials)])
-def list_assets(uid: str = Depends(validate_credentials)):
+def list_assets(uid: str = Depends(validate_credentials), access_point: str = Query(None), action: str = Query(None)) \
+        -> JSONResponse:
     """
-    Retrieves assets for a user.
+    Retrieves information about available assets for a user.
 
     Parameters:
     - **uid** (str): The User ID of the validated user.
+    - **access_point** (str): The access_point name to filter assets by. If None all are searched.
+    - **action** (str): the action type ("read" or "write") to filter assets by. If None both are searched.
 
     Returns:
-    - **dict**: The assets for the user.
+    - **JSONResponse**: A JSON containing:
+        - **assets**: A list of the asset keys the user has access to according to the provided filters.
 
     Raises:
     - **HTTPException**: If the provided credentials are invalid.
     """
-    # Retrieve assets for user_id
-    assets = dam.get_all_user_assets(uid)
+    # Retrieve policies according to the filters
+    policies = dam.get_all_user_policies(uid, access_point, action)
+
+    # Retrieve all file names according to the filtered policies.
+    assets = {agent: [] for agent in set(policies.keys())}
+    for agent in policies.keys():
+        for asset_reg_ex in policies[agent]:
+            assets[agent].append(agents[agent].get_file_paths(asset_reg_ex))
 
     # Valid Response
-    return {"assets": assets}
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"assets": assets})
 
 
 @user_router.put("/upload", dependencies=[Depends(validate_credentials)])
-def put_asset(uid: str = Depends(validate_credentials), resource: str = Query(...), access_point: str = Query(...)) -> dict:
+def put_asset(uid: str = Depends(validate_credentials), resource: str = Query(...), access_point: str = Query(...)) -> (
+        JSONResponse):
     """
     Sends a PUT request to the asset router to generate a presigned URL for uploading a file.
 
@@ -45,7 +59,9 @@ def put_asset(uid: str = Depends(validate_credentials), resource: str = Query(..
     - **access_point** (str): The name of the storage access point where the resource is located.
 
     Returns:
-    - **dict**: The presigned URL for uploading the file.
+    - **JSONResponse**: A JSON containing:
+        - **presigned_urls**: A list of presigned URL for uploading the file(s).
+        - **file_paths**: A list of file paths for uploading the file(s).
 
     Raises:
     - **HTTPException**: If the asset router URL is not found or the file cannot be uploaded (status code 404).
@@ -58,7 +74,8 @@ def put_asset(uid: str = Depends(validate_credentials), resource: str = Query(..
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Access point {access_point} not found.")
 
         presigned_urls, file_paths = agent.generate_access_links(f"{resource}", 'PUT', 3600)
-        return {"presigned_urls": presigned_urls, 'file_paths': file_paths}
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content={"presigned_urls": presigned_urls, 'file_paths': file_paths})
 
     else:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
@@ -66,7 +83,8 @@ def put_asset(uid: str = Depends(validate_credentials), resource: str = Query(..
 
 
 @user_router.put("/download", dependencies=[Depends(validate_credentials)])
-def get_asset(uid: str = Depends(validate_credentials), resource: str = Query(...), access_point: str = Query(...)) -> dict:
+def get_asset(uid: str = Depends(validate_credentials), resource: str = Query(...), access_point: str = Query(...)) \
+        -> JSONResponse:
     """
     Get presigned URLs for downloading the specified asset.
 
@@ -76,7 +94,8 @@ def get_asset(uid: str = Depends(validate_credentials), resource: str = Query(..
     - **access_point** (str): The name of the storage access point where the resource is located.
 
     Returns:
-    - **dict**: The presigned URLs for the asset.
+    - **JSONResponse**: A dictionary of the presigned URLs for the asset. Keys are the access point slugs, value is a
+        list of the corresponding presigned urls for assets stored at that access point.
 
     Raises:
     - **HTTPException**: If the asset is not found (status code 404).
@@ -91,7 +110,8 @@ def get_asset(uid: str = Depends(validate_credentials), resource: str = Query(..
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Access point {access_point} not found.")
 
         presigned_urls, file_paths = agent.generate_access_links(resource, 'GET', 600)
-        return {"presigned_urls": presigned_urls, 'file_paths': file_paths}
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content={"presigned_urls": presigned_urls, 'file_paths': file_paths})
 
     else:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
