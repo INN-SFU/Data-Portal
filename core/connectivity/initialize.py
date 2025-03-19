d5a22e940048295db39efe7039a12658337ed5fb
pmahon3 <pmahon3@uwo.ca>
2025-03-18 17:21:59 -0700

    Reconfigured storage agents to a factory design pattern; factory creates endpoints via creating an storage agent according to the endpoint config file.

--- core/connectivity/initialize.py
+++
@@ -1,5 +0,0 @@
-from core.connectivity.arbutus import ArbutusAgent
-
-# Initialize Data Access Point Agents
-arbutus_agent = ArbutusAgent()
-agents = {arbutus_agent.access_point_slug: arbutus_agent}
