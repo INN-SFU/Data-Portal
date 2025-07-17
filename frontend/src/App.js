8cbbfd8758d24f0ec42eb774c6956a73021d9393
aensan <15043533+AEnsan@users.noreply.github.com>
2025-07-17 10:18:15 -0700

    Created React App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          New React app
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
