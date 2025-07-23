a9f3dde7e191411112d8c93aa7c6797ff7ae960a
AEnsan <15043533+AEnsan@users.noreply.github.com>
2025-07-22 17:44:42 -0700

    Cleaned up keyckloak settings

--- frontend/setting/Keycloak.js
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = new Keycloak({
-  url: process.env.REACT_APP_KeyCloakServerUrl,// Keycloak base URL
-  realm: process.env.REACT_APP_KeyCloakRealm, // realm name
-  clientId: process.env.REACT_APP_KeyCloakClientId, // client ID
-});
-
-export default keycloak;
